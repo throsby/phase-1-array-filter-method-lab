2377426fe01ec7403c8ab56ffa293abe9dac5018
Throsby Wells <throsbywells@gmail.com>
2022-03-28 10:08:19 -0400

    commit tree 1f3277fc2202f18e4bb15ed20fe9a90540f4d7c6

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solution here
+
+function findMatching(arr,str) {
+    return arr.filter((e) => {
+        return str.toLowerCase() === e.toLowerCase()
+    })
+}
+
+function fuzzyMatch(arr,str) {
+    return arr.filter((e)=> {
+        console.log(e.substring(0,1))
+        return e.substring(0,1).toLowerCase() === str.substring(0,1).toLowerCase()
+    })
+}
+
+function matchName(arr,str) {
+    return arr.filter((e) => {       
+        return e.name.toLowerCase() === str.toLowerCase()
+    })
+}
